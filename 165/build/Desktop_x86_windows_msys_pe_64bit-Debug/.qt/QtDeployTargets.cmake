4578b11ac32ca5f6e5f88fed2ef13c53b5f0b877
Liam Morgan <lmorgan@calamu.com>
2024-04-21 11:38:59 -0700

    restart

--- 165/build/Desktop_x86_windows_msys_pe_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_snakegame_FILE C:/Users/daisy/OneDrive/Documents/Test Studio Projects/CSE165Project/165/build/Desktop_x86_windows_msys_pe_64bit-Debug/snakegame.exe)
-set(__QT_DEPLOY_TARGET_snakegame_RUNTIME_DLLS D:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.7.0/mingw_64/bin/Qt6OpenGL.dll;D:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
